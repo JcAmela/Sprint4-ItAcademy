868eeeceb57f2e541e9ffc030d04d804ad026cd3
JcAmela <juancarlosamela@gmail.com>
2023-06-07 10:16:17 +0200

    first exercise finished

--- src/films.js
+++ src/films.js
@@ -1,58 +1,57 @@
-// Exercise 1: Get the array of all directors.
+// Ejercicio 1: Obtén el array de todos los directores.
 function getAllDirectors(array) {
-  let result =  "???";
-  console.log("EXERCICE 1 ->", result);
+  let result = [];
+  result = array.map(element => element.director);
+  console.log("EJERCICIO 1 ->", result);
   return result;
-}
-
-// Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
- 
-}
-
-// Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
+  }
   
-}
-
-// Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
+  // Ejercicio 2: Obtén las películas de un director específico.
+  function getMoviesFromDirector(array, director) {
   
-}
-
-// Exercise 5: Order by year, ascending
-function orderByYear() {
-
-}
-
-// Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
-}
-
-// Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-
-}
-
-// Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
+  }
   
-}
-
-
-
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
+  // Ejercicio 3: Calcula el promedio de las películas de un director dado.
+  function moviesAverageOfDirector(array, director) {
+  
+  }
+  
+  // Ejercicio 4: Ordena alfabéticamente por título.
+  function orderAlphabetically(array) {
+  
+  }
+  
+  // Ejercicio 5: Ordena por año, de forma ascendente.
+  function orderByYear() {
+  
+  }
+  
+  // Ejercicio 6: Calcula el promedio de las películas en una categoría.
+  function moviesAverageByCategory() {
+  
+  }
+  
+  // Ejercicio 7: Modifica la duración de las películas a minutos.
+  function hoursToMinutes() {
+  
+  }
+  
+  // Ejercicio 8: Obtén la mejor película de un año.
+  function bestFilmOfYear() {
+  
+  }
+  
+  // Lo siguiente es necesario para que funcionen las pruebas unitarias.
+  /* Configuración del entorno. No modificar el código siguiente. */
+  if (typeof module !== 'undefined') {
   module.exports = {
-    getAllDirectors,
-    getMoviesFromDirector,
-    moviesAverageOfDirector,
-    orderAlphabetically,
-    orderByYear,
-    moviesAverageByCategory,
-    hoursToMinutes,
-    bestFilmOfYear,
+  getAllDirectors,
+  getMoviesFromDirector,
+  moviesAverageOfDirector,
+  orderAlphabetically,
+  orderByYear,
+  moviesAverageByCategory,
+  hoursToMinutes,
+  bestFilmOfYear,
   };
-}
+  }
