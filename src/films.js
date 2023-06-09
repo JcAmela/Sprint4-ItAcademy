54d27ca650f21c4e87f652a7a9c0f17247d90d7c
JcAmela <juancarlosamela@gmail.com>
2023-06-09 13:50:12 +0200

    Código ITA

--- src/films.js
+++ src/films.js
@@ -16,22 +16,37 @@ function getAllDirectors(array) {
   // Ejercicio 3: Calcula el promedio de las películas de un director dado.
   function moviesAverageOfDirector(array, director) {
     let directoresEscogidos = array.filter(e => e.director === director)
-    let reducido = directoresEscogidos.reduce((total, valor) => {
+    let scoresDirector = directoresEscogidos.map(pelicula => pelicula.score)
+    let reducido = scoresDirector.reduce((total, valor) => {
       return total + valor;
     }, 0);
-    let result = reducido / array.lenght
-    console.log("EJERCICIO 3 ->", result);
+    let result = reducido / directoresEscogidos.length
+    console.log("EJERCICIO 3 ->", result.toFixed(2));
     return result;
   }
   
   // Ejercicio 4: Ordena alfabéticamente por título.
   function orderAlphabetically(array) {
-  
+    let arraySort= array.slice()
+    let sorteado = arraySort.sort((a, b) => a.title.localeCompare(b.title));
+    let mapeado = sorteado.map (e=> e.title)
+    let result = mapeado.slice(0, 20)
+  console.log("EJERCICIO 4 ->", result);
+  return result;
   }
-  
+
   // Ejercicio 5: Ordena por año, de forma ascendente.
-  function orderByYear() {
-  
+  function orderByYear(array) {
+    let newArray= [...array]
+    let result = newArray.sort((a, b) => {
+      if(a.year === b.year){
+        return a.title.localeCompare(b.title);
+    } else {
+      return a.year - b.year;
+    }
+  });
+    console.log("EJERCICIO 5 ->", result);
+    return result;
   }
   
   // Ejercicio 6: Calcula el promedio de las películas en una categoría.
